b7c41e4f58becb7a7e17c1a56a80ea12c297195b
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-02-16 17:19:57 -0500

    Fixed sorting messing up the group indexes

--- main.js
+++ main.js
@@ -770,6 +770,11 @@ sortCss.each['sass include block'] = function (settings, element) {
 
 sortCss.list['sass variable assignment'] = function (settings, list) {
   list.sort(smartSort('name'));
+  for (var i = 0, n = list.length; i < n; i++) {
+    list[i].groupIndex = i;
+    list[i].first = i === 0;
+    list[i].last = i === n - 1;
+  }
 };
 
 function getValue(settings, cssObject) {
