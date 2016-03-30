f69d4495100fae85c7becb66c5ff2abb091ee0c7
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-03-30 09:31:52 -0400

    titling support for comments

--- main.js
+++ main.js
@@ -1015,20 +1015,36 @@ getValue.selector = function (settings, element, parent) {
 
 getValue['comment block'] = function (settings, element, parent) {
   var tab = new Array((element.depth * settings.tabSize) + 1).join(settings.tabChar);
+  var initValue = element.value.slice();
   var value;
+
+  function isTitle() {
+    /*
+      Titling support
+      http://cssguidelin.es/#titling
+    */
+    var start = /^[\-]+(\s+|)\*\\$/.test(initValue[0]);
+    var end = /^\\\*(\s+|)[\-]+$/.test(initValue.slice(-1)[0]);
+    return start && end;
+  }
+
   function lineBreak() {
     var lines = [[]];
     var $tab = new Array(settings.tabSize + 1).join(settings.tabChar);
     var x = 0;
-    var v = element.value.join('\n').split(' ').filter((a) => a.length);
+    var v = element.value.join('\n').split(' ').filter(function (a) { return a.length; });
     var tabLength = tab.length + $tab.length;
-    for (var i = v.length - 1; i >= 0; i--) {
+    var i;
+    var n;
+
+    for (i = v.length - 1; i >= 0; i--) {
       if (v[i].length > 1 && v[i].substr(-1) === '\n') {
         v[i] = lasso.trimEnd(v[i]);
         v.splice(i + 1, 0, '\n');
       }
     }
-    for (var i = 0, n = v.length; i < n; i++) {
+
+    for (i = 0, n = v.length; i < n; i++) {
       if (v[i] === '\n') {
         x = 0;
       } else {
@@ -1039,6 +1055,7 @@ getValue['comment block'] = function (settings, element, parent) {
         }
       }
     }
+
     if (v.length) {
       element.value = v.reduce(function (a, b) {
         if (b === '\n') {
@@ -1050,17 +1067,35 @@ getValue['comment block'] = function (settings, element, parent) {
       }).split('\n');
     }
   }
+
   if (settings.lineBreak) {
     lineBreak();
   }
+
   if (element.value.length > 1) {
+    if (isTitle()) {
+      value = element.value.map(function (line, i) {
+        var $tab = '';
+
+        if (i > 0 && i < element.value.length - 1) {
+          $tab = new Array(settings.tabSize + 1).join(settings.tabChar);
+        }
+        
+        return $tab + tab + line;
+      }).join('\n');
+
+      return tab + '/*' + value + tab + '*/';
+    }
+
     value = element.value.map(function (line) {
       var $tab = new Array(settings.tabSize + 1).join(settings.tabChar);
-      return `${$tab}${tab}${line}`;
+      return $tab + tab + line;
     }).join('\n');
-    return `${tab}/*\n${value}\n${tab}*/`;
+
+    return tab + '/*\n' + value + '\n' + tab + '*/';
   }
-  return `${tab}/*\ ${element.value.join('\n')} */`
+
+  return tab + '/*' + element.value.join('\n') + '*/';
 };
 
 getValue['comment inline'] = function (settings, element, parent) {
--- src/css-clean-src/value/getValue.comment-block.js
+++ src/css-clean-src/value/getValue.comment-block.js
@@ -1,19 +1,35 @@
 getValue['comment block'] = function (settings, element, parent) {
   var tab = new Array((element.depth * settings.tabSize) + 1).join(settings.tabChar);
+  var initValue = element.value.slice();
   var value;
+
+  function isTitle() {
+    /*
+      Titling support
+      http://cssguidelin.es/#titling
+    */
+    var start = /^[\-]+(\s+|)\*\\$/.test(initValue[0]);
+    var end = /^\\\*(\s+|)[\-]+$/.test(initValue.slice(-1)[0]);
+    return start && end;
+  }
+
   function lineBreak() {
     var lines = [[]];
     var $tab = new Array(settings.tabSize + 1).join(settings.tabChar);
     var x = 0;
-    var v = element.value.join('\n').split(' ').filter((a) => a.length);
+    var v = element.value.join('\n').split(' ').filter(function (a) { return a.length; });
     var tabLength = tab.length + $tab.length;
-    for (var i = v.length - 1; i >= 0; i--) {
+    var i;
+    var n;
+
+    for (i = v.length - 1; i >= 0; i--) {
       if (v[i].length > 1 && v[i].substr(-1) === '\n') {
         v[i] = lasso.trimEnd(v[i]);
         v.splice(i + 1, 0, '\n');
       }
     }
-    for (var i = 0, n = v.length; i < n; i++) {
+
+    for (i = 0, n = v.length; i < n; i++) {
       if (v[i] === '\n') {
         x = 0;
       } else {
@@ -24,6 +40,7 @@ getValue['comment block'] = function (settings, element, parent) {
         }
       }
     }
+
     if (v.length) {
       element.value = v.reduce(function (a, b) {
         if (b === '\n') {
@@ -35,15 +52,33 @@ getValue['comment block'] = function (settings, element, parent) {
       }).split('\n');
     }
   }
+
   if (settings.lineBreak) {
     lineBreak();
   }
+
   if (element.value.length > 1) {
+    if (isTitle()) {
+      value = element.value.map(function (line, i) {
+        var $tab = '';
+
+        if (i > 0 && i < element.value.length - 1) {
+          $tab = new Array(settings.tabSize + 1).join(settings.tabChar);
+        }
+        
+        return $tab + tab + line;
+      }).join('\n');
+
+      return tab + '/*' + value + tab + '*/';
+    }
+
     value = element.value.map(function (line) {
       var $tab = new Array(settings.tabSize + 1).join(settings.tabChar);
-      return `${$tab}${tab}${line}`;
+      return $tab + tab + line;
     }).join('\n');
-    return `${tab}/*\n${value}\n${tab}*/`;
+
+    return tab + '/*\n' + value + '\n' + tab + '*/';
   }
-  return `${tab}/*\ ${element.value.join('\n')} */`
+
+  return tab + '/*' + element.value.join('\n') + '*/';
 };
