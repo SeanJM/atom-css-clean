cbe815171e2b8ada1365a126bb4402359908293e
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-04-20 17:20:17 -0400

    Removed unnecessary line

--- src/css-clean-src/value/getValue.property-group.js
+++ src/css-clean-src/value/getValue.property-group.js
@@ -1,6 +1,5 @@
 getValue['property group'] = function (settings, element, parent) {
   var value = splitByComma(element.value);
-  var commas = lasso.indexesOf(element.value, ',');
   var captures;
   var tab = new Array((element.depth * settings.tabSize) + 1).join(settings.tabChar);
   if (element.align) {
