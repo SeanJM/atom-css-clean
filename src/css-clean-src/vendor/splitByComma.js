c65d90fa8c3427cf24deeec5ae4f0cfe6d284576
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-04-20 17:20:33 -0400

    Rewrote 'splitByComma' which fixed a bug.

--- src/css-clean-src/vendor/splitByComma.js
+++ src/css-clean-src/vendor/splitByComma.js
@@ -1,26 +1,24 @@
 function splitByComma (value) {
-  var sublist = lasso.between(value, '(', ')');
-  var commas = lasso.indexesOf(value, ',');
-  var lines = [''];
+  var open = 0;
   var index = 0;
-  if (sublist.length) {
-    commas = commas.filter(function (comma) {
-      for (var i = 0, n = sublist.length; i < n; i++) {
-        if (comma.index > sublist[i].index && comma.index < sublist[i].index + sublist[i].length) {
-          return false;
-        }
-      }
-      return comma.index > sublist.slice(-1)[0].index;
-    }).map((a) => a.index);
-    for (var i = 0, n = value.length; i < n; i++) {
-      if (commas.indexOf(i) !== -1) {
-        index += 1;
-        lines[index] = '';
-      } else {
-        lines[index] += value[i];
-      }
+  var i = 0;
+  var n = value.length;
+  var lines = [''];
+
+  for (; i < n; i++) {
+    if (value[i] === '(') {
+      open += 1;
+      lines[index] += value[i];
+    } else if (value[i] === ')') {
+      open -= 1;
+      lines[index] += value[i];
+    } else if (value[i] === ',' && open === 0) {
+      index += 1;
+      lines[index] = '';
+    } else {
+      lines[index] += value[i];
     }
-    return lines.map((a) => a.trim());
   }
-  return value.split(',').map((a) => a.trim());
-};
+
+  return lines.map(function (a) { return a.trim(); });
+}
