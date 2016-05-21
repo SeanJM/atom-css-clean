630c2fb5540e88103c0f005d532e91072f6499d8
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-05-21 10:40:08 -0400

    Removed sorting of variable assignment

--- src/css-clean-src/sortCss/sortCss.list.sass-variable-assignment.js
+++
@@ -1,8 +0,0 @@
-sortCss.list['sass variable assignment'] = function (settings, list) {
-  list.sort(smartSort('name'));
-  for (var i = 0, n = list.length; i < n; i++) {
-    list[i].groupIndex = i;
-    list[i].first = i === 0;
-    list[i].last = i === n - 1;
-  }
-};
