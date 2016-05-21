98a667bb36a678855ab1a50461b704750b49c424
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-05-21 09:45:13 -0400

    Moved 'alignTogether' into 'cleanCss.js'

--- src/css-clean-src/css-clean/cleanCss.js
+++ src/css-clean-src/css-clean/cleanCss.js
@@ -1,9 +1,14 @@
 function cleanCss(string) {
   var settings = {
-    lineBreak : 80,
-    align : false,
-    sortMainScope : false,
-    sortBlockScope : true,
+    alignTogether : [
+      'property group',
+      'sass import',
+      'sass include',
+      'sass include arguments',
+      'sass extend',
+      'sass variable assignment',
+    ],
+
     blockScopeOrder : [
       'sass import',
       'sass variable assignment',
@@ -15,6 +20,11 @@ function cleanCss(string) {
       'selector',
       'sass return'
     ],
+    
+    lineBreak : 80,
+    align : false,
+    sortMainScope : false,
+    sortBlockScope : true,
     propertyGroupOrder : list.properties,
     tabSize : 2
   };
