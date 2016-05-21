bf33de8661a8d2ff1dfa415929c86679eb7c8cc0
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-05-21 09:53:27 -0400

    Declare 'displace' arrays at the top of the function

--- src/css-clean-src/sortCss/sortCss.js
+++ src/css-clean-src/sortCss/sortCss.js
@@ -1,35 +1,42 @@
 function sortCss(settings, cssObject) {
+  var displaceDeep = [
+    'sass function',
+    'sass import',
+    'sass include',
+    'sass include arguments',
+    'sass mixin',
+    'sass include block',
+    'sass extend',
+    'property group',
+  ];
+
+  var displaceZeroDepth = [
+    'sass import',
+    'sass include',
+    'sass variable assignment',
+    'font face',
+    'sass function',
+    'sass mixin',
+    'sass include block',
+    'sass placeholder',
+  ];
+
   function sortDeep(array) {
     sortCss.scope(settings, array, {
-      displace : [
-        'sass function',
-        'sass import',
-        'sass include',
-        'sass include arguments',
-        'sass mixin',
-        'sass include block',
-        'sass extend',
-        'property group',
-      ]
+      displace : displaceDeep
     });
+
     for (var i = 0, n = array.length; i < n; i++) {
       if (Array.isArray(array[i].content) && array[i].content.length) {
         sortDeep(array[i].content);
       }
     }
   }
+
   sortCss.scope(settings, cssObject, {
-    displace : [
-      'sass import',
-      'sass include',
-      'sass variable assignment',
-      'font face',
-      'sass function',
-      'sass mixin',
-      'sass include block',
-      'sass placeholder',
-    ]
+    displace : displaceZeroDepth
   });
+
   if (settings.sortBlockScope) {
     for (var i = 0, n = cssObject.length; i < n; i++) {
       if (Array.isArray(cssObject[i].content) && cssObject[i].content.length) {
