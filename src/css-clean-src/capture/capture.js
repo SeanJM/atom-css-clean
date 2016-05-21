fa483d146646a1a35af239eafb026a5903c3c71c
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-05-21 09:44:55 -0400

    Moved the functions which would add 'index', 'length', 'depth' etc. properties to a CSS group into 'getValue', because it was useless as it was happening before sorting.

--- src/css-clean-src/capture/capture.js
+++ src/css-clean-src/capture/capture.js
@@ -4,15 +4,6 @@ function capture(string, group, depth) {
   var n;
   var c;
   var stackOverFlowIntMax = 10000;
-  var scopeCount = {};
-  var alignTogether = [
-    'property group',
-    'sass import',
-    'sass include',
-    'sass include arguments',
-    'sass extend',
-    'sass variable assignment',
-  ];
 
   function getScope(string) {
     if (string.substr(0, 2) === '//') {
@@ -95,7 +86,6 @@ function capture(string, group, depth) {
 
     c = capture[scope](string, {
       group : group,
-      depth : depth,
       scope : scope
     });
 
@@ -108,47 +98,6 @@ function capture(string, group, depth) {
     throw 'CSS Clean has stopped: There must be a problem with your CSS.';
   }
 
-  for (i = 0, n = group.length; i < n; i++) {
-    // untracked scopes & ''@if' starts a new group
-    if (typeof scopeCount[group[i].scope] === 'undefined' || group[i].name === '@if') {
-      scopeCount[group[i].scope] = {
-        groupLength : 1,
-        align : group[i].name ? group[i].name.length : 0
-      };
-    } else {
-      scopeCount[group[i].scope].groupLength += 1;
-      if (group[i].name && scopeCount[group[i].scope].align < group[i].name.length) {
-        scopeCount[group[i].scope].align = group[i].name.length;
-      }
-    }
-    group[i].first = scopeCount[group[i].scope].groupLength === 1;
-    group[i].groupIndex = scopeCount[group[i].scope].groupLength - 1;
-    group[i].index = i;
-    group[i].length = n;
-  }
-
-  for (i = 0, n = group.length; i < n; i++) {
-    group[i].groupLength = scopeCount[group[i].scope].groupLength;
-    group[i].last = group[i].groupIndex === group[i].groupLength - 1;
-  }
-  if (depth === 0) {
-    for (i = 0, n = group.length; i < n; i++) {
-      group[i].align = scopeCount[group[i].scope].align;
-    }
-  } else {
-    // Get alignments
-    alignTogether.align = 0;
-    for (i = 0, n = alignTogether.length; i < n; i++) {
-      if (scopeCount[alignTogether[i]] && scopeCount[alignTogether[i]].align > alignTogether.align) {
-        alignTogether.align = scopeCount[alignTogether[i]].align;
-      }
-    }
-    for (i = 0, n = group.length; i < n; i++) {
-      if (alignTogether.indexOf(group[i].scope) !== -1) {
-        group[i].align = alignTogether.align;
-      }
-    }
-  }
   return group;
 }
 
--- src/css-clean-src/value/getValue.js
+++ src/css-clean-src/value/getValue.js
@@ -1,9 +1,89 @@
 function getValue(settings, cssObject) {
+  var i;
+  var n;
+
+  function indexSiblings(group, depth) {
+    var scopeCount = {};
+    var align = 0;
+    var i = 0;
+    var n = group.length;
+    var x;
+
+    for (; i < n; i++) {
+      // untracked scopes & ''@if' starts a new group
+      x = group[i];
+
+      if (typeof scopeCount[x.scope] === 'undefined' || x.name === '@if') {
+        scopeCount[x.scope] = {
+          groupLength : 1,
+          align : x.name ? x.name.length : 0
+        };
+      } else {
+        scopeCount[x.scope].groupLength += 1;
+
+        if (x.name && scopeCount[x.scope].align < x.name.length) {
+          scopeCount[x.scope].align = x.name.length;
+        }
+      }
+
+      x.first = scopeCount[x.scope].groupLength === 1;
+      x.groupIndex = scopeCount[x.scope].groupLength - 1;
+
+      x.index = i;
+      x.length = n;
+      x.depth = depth;
+    }
+
+    i = 0;
+
+    for (; i < n; i++) {
+      x = group[i];
+
+      x.groupLength = scopeCount[x.scope].groupLength;
+      x.last = x.groupIndex === x.groupLength - 1;
+
+      if (x.depth === 0) {
+        x.align = scopeCount[x.scope].align;
+      }
+    }
+
+    // Get alignments for groups that align together
+    i = 0;
+    n = settings.alignTogether.length;
+
+    for (; i < n; i++) {
+      x = settings.alignTogether[i];
+      if (scopeCount[x] && scopeCount[x].align > align) {
+        align = scopeCount[x].align;
+      }
+    }
+
+    i = 0;
+    n = group.length;
+
+    for (; i < n; i++) {
+      x = group[i];
+
+      if (settings.alignTogether.indexOf(x.scope) !== -1) {
+        x.align = align;
+      }
+
+      // Recursively get information about siblings
+      if (Array.isArray(x.content)) {
+        indexSiblings(x.content, depth + 1);
+      }
+    }
+  }
+
+  indexSiblings(cssObject, 0);
+
   return getValue.map(settings, cssObject).map(function (value, i) {
     var element = cssObject[i];
+
     if (element.scope === 'sass variable assignment' && !element.last) {
       return value;
     }
+
     return value + '\n';
   }).join('\n');
 }
