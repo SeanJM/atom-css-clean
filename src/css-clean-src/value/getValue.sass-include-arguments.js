6cc5df642097684487e609ddb8d33836bff286aa
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-02 08:22:53 -0400

    Fixed alignment for '@include' since they are no longer grouped together.

--- src/css-clean-src/value/getValue.sass-include-arguments.js
+++ src/css-clean-src/value/getValue.sass-include-arguments.js
@@ -1,13 +1,10 @@
 getValue['sass include arguments'] = function (settings, element, parent) {
   var args = '(' + element.arguments.join(', ') + ')';
-  var align;
-  if (element.depth === 0) {
-    align = new Array(element.align - element.name.length + 2).join(' ');
-  } else {
-    align = new Array(element.align - element.name.length + 4).join(' ');
-  }
-  if (element.align && parent.length > 1) {
+  var align = new Array(element.align - element.name.length + 4).join(' ');
+
+  if (element.depth && element.align && parent.length > 1) {
     return `${element.name}${align}${element.value}${args};`;
   }
+
   return `${element.name} ${element.value}${args};`;
 };
