6f0f4ae413c6f276dd1cb0f474568638e2b1ba10
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-04 13:46:38 -0400

    Fixed direct child selector bug

--- main.js
+++ main.js
@@ -627,7 +627,7 @@ is.propertyGroup = function (value) {
 };
 
 is.selector = function (value) {
-  var selector = /^([\@\.\%\#\*a-zA-Z0-9\[\]\+\~\=\"\'\_\-\:\&\n,\(\) ]+)/.test(value);
+  var selector = /^([\>\@\.\%\#\*a-zA-Z0-9\[\]\+\~\=\"\'\_\-\:\&\n,\(\) ]+)/.test(value);
   var braceIndex = value.indexOf('{');
   var semiIndex = value.indexOf(';');
   var braceBefore = semiIndex > braceIndex && braceIndex !== -1 || semiIndex === -1 && braceIndex !== -1;
--- src/css-clean-src/is/is.selector.js
+++ src/css-clean-src/is/is.selector.js
@@ -1,5 +1,5 @@
 is.selector = function (value) {
-  var selector = /^([\@\.\%\#\*a-zA-Z0-9\[\]\+\~\=\"\'\_\-\:\&\n,\(\) ]+)/.test(value);
+  var selector = /^([\>\@\.\%\#\*a-zA-Z0-9\[\]\+\~\=\"\'\_\-\:\&\n,\(\) ]+)/.test(value);
   var braceIndex = value.indexOf('{');
   var semiIndex = value.indexOf(';');
   var braceBefore = semiIndex > braceIndex && braceIndex !== -1 || semiIndex === -1 && braceIndex !== -1;
