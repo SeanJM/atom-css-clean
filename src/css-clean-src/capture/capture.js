e8ab9f37ff34f3c2eb645935696348df4a2242a1
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-16 12:25:20 -0400

    Cleaned up 'capture' function

--- src/css-clean-src/capture/capture.js
+++ src/css-clean-src/capture/capture.js
@@ -1,104 +1,126 @@
-function capture(string, group, depth) {
-  var scope;
-  var i = 0;
-  var n;
-  var c;
-  var stackOverFlowIntMax = 10000;
-
-  function getScope(string) {
-    if (string.substr(0, 2) === '//') {
+(function () {
+  function getScope(value) {
+    if (value.substr(0, 2) === '//') {
       return 'comment inline';
     }
-    if (string.substr(0, 2) === '/*') {
+
+    if (value.substr(0, 2) === '/*') {
       return 'comment block';
     }
-    if (/^\$[^:]+?:[^;]+?;/.test(string)) {
+
+    if (/^\$[^:]+?:[^;]+?;/.test(value)) {
       return 'sass variable assignment';
     }
-    if (string.substring(0, 7) === '@extend') {
+
+    if (value.substring(0, 7) === '@extend') {
       return 'sass extend';
     }
-    if (string.substring(0, 7) === '@import') {
+
+    if (value.substring(0, 7) === '@import') {
       return 'sass import';
     }
-    if (string.substring(0, 8) === '@include') {
-      if (/^@include\s+[a-zA-Z0-9\-\_\s]+\{/.test(string)) {
+
+    if (value.substring(0, 8) === '@include') {
+      if (/^@include\s+[a-zA-Z0-9\-\_\s]+\{/.test(value)) {
         return 'sass include block';
       }
-      if (/^@include\s+[a-zA-Z0-9\-\_\s]+\(/.test(string)) {
+      if (/^@include\s+[a-zA-Z0-9\-\_\s]+\(/.test(value)) {
         return 'sass include arguments';
       }
       return 'sass include';
     }
-    if (string.substring(0, 6) === '@mixin') {
+
+    if (value.substring(0, 6) === '@mixin') {
       return 'sass mixin';
     }
-    if (string.substring(0, 9) === '@function') {
+
+    if (value.substring(0, 9) === '@function') {
       return 'sass function';
     }
-    if (string.substring(0, 7) === '@return') {
+
+    if (value.substring(0, 7) === '@return') {
       return 'sass return';
     }
-    if (string.substring(0, 3) === '@if') {
+
+    if (value.substring(0, 3) === '@if') {
       return 'sass if';
     }
-    if (string.substring(0, 4) === '@for') {
+
+    if (value.substring(0, 4) === '@for') {
       return 'sass for';
     }
-    if (string.substring(0, 5) === '@each') {
+
+    if (value.substring(0, 5) === '@each') {
       return 'sass each';
     }
-    if (/^@else[ ]+if/.test(string)) {
+
+    if (/^@else[ ]+if/.test(value)) {
       return 'sass if';
     }
-    if (string.substring(0, 5) === '@else') {
+
+    if (value.substring(0, 5) === '@else') {
       return 'sass if';
     }
-    if (string.substring(0, 10) === '@font-face') {
+
+    if (value.substring(0, 10) === '@font-face') {
       return 'font face';
     }
-    if (is.selector(string) && /^%|^[^\%^{]+?%[^\{]+?\{/.test(string)) {
+
+    if (is.selector(value) && /^%|^[^\%^{]+?%[^\{]+?\{/.test(value)) {
       return 'sass placeholder';
     }
-    if (string.substring(0, 6) === '@media') {
+
+    if (value.substring(0, 6) === '@media') {
       return 'media query';
     }
-    if (string.substring(0, 8) === '@charset') {
+
+    if (value.substring(0, 8) === '@charset') {
       return 'character set';
     }
-    if (is.propertyGroup(string)) {
+
+    if (is.propertyGroup(value)) {
       return 'property group';
     }
-    if (is.selector(string)) {
+
+    if (is.selector(value)) {
       return 'selector';
     }
+
     // In the future return 'unknown'
     return false;
   }
 
-  string = string.trim();
-  scope = getScope(string);
+  function capture(value, group, depth) {
+    var scope = getScope(value);
+    var i = 0;
+    var c;
+    var stackOverFlowIntMax = 10000;
 
-  while (i++ < stackOverFlowIntMax && scope) {
-    if (typeof capture[scope] === 'undefined') {
-      throw 'Missing \'' + scope + '\' method for \'capture\' function.';
+    while (i++ < stackOverFlowIntMax && scope) {
+      if (typeof capture[scope] === 'undefined') {
+        throw 'Missing \'' + scope + '\' method for \'capture\' function.';
+      }
+
+      c = capture[scope](value, {
+        group : group,
+        scope : scope
+      });
+
+      group.push(c);
+      value = value.slice(c.strlen).trim();
+      scope = getScope(value);
     }
 
-    c = capture[scope](string, {
-      group : group,
-      scope : scope
-    });
+    if (i === stackOverFlowIntMax) {
+      throw 'CSS Clean has stopped: There must be a problem with your CSS.';
+    }
 
-    group.push(c);
-    string = string.slice(c.strlen).trim();
-    scope = getScope(string);
+    return group;
   }
 
-  if (i === stackOverFlowIntMax) {
-    throw 'CSS Clean has stopped: There must be a problem with your CSS.';
+  capture.shared = {};
+
+  if (module) {
+    module.exports = capture;
   }
-
-  return group;
-}
-
-capture.shared = {};
+}());
