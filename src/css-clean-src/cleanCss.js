2d236c9ce4927379c548757dd6a8cca537a7bc48
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-19 10:06:31 -0400

    Merged 'cleanCss' prototypes

--- src/css-clean-src/cleanCss.js
+++ src/css-clean-src/cleanCss.js
@@ -1,70 +1,69 @@
-(function () {
-  function CleanCss(string) {
+function CleanCss(string) {
 
-    this.alignTogether = [
-      'property group',
-      'sass import',
-      'sass include',
-      'sass include arguments',
-      'sass extend',
-      'sass variable assignment',
-    ];
+  this.alignTogether = [
+    'property group',
+    'sass import',
+    'sass include',
+    'sass include arguments',
+    'sass extend',
+    'sass variable assignment',
+  ];
 
-    this.blockScopeOrder = [
-      'sass import',
-      'sass variable assignment',
-      'sass extend',
-      'sass include',
-      'sass include arguments',
-      'property group',
-      'sass include block',
-      'selector',
-      'sass return'
-    ];
+  this.blockScopeOrder = [
+    'sass import',
+    'sass variable assignment',
+    'sass extend',
+    'sass include',
+    'sass include arguments',
+    'property group',
+    'sass include block',
+    'selector',
+    'sass return'
+  ];
 
-    this.lineBreak = 80;
-    this.align = false;
-    this.sortMainScope = false;
-    this.sortBlockScope = true;
-    this.propertyGroupOrder = list.properties;
-    this.tabSize = 2;
+  this.lineBreak = 80;
+  this.align = false;
+  this.sortMainScope = false;
+  this.sortBlockScope = true;
+  this.propertyGroupOrder = list.properties;
+  this.tabSize = 2;
+  // This is wrapped this way so that when the object is passed, the string
+  // can be mutated -- yes, I want mutation in this case.
+  this.value = { string : string.trim() };
+}
 
-    this.value = string.trim();
-  }
+CleanCss.prototype.align = function () {
+  this.align = true;
+};
 
-  CleanCss.prototype.align = function () {
-    this.align = true;
-  };
+CleanCss.prototype.indent = function (length, type) {
+  this.tabChar = type === 'space'
+    ? ' '
+    : type === 'tab'
+      ? '\t'
+      : undefined;
+};
 
-  CleanCss.prototype.indent = function (length, type) {
-    this.tabChar = type === 'space'
-      ? ' '
-      : type === 'tab'
-        ? '\t'
-        : undefined;
-  };
+CleanCss.prototype.setLineBreak = function (length) {
+  this.lineBreak = length;
+};
 
-  CleanCss.prototype.setLineBreak = function (length) {
-    this.lineBreak = length;
-  };
+CleanCss.prototype.sortBlockScope = function () {
+  this.sortBlockScope = true;
+};
 
-  CleanCss.prototype.sortBlockScope = function () {
-    this.sortBlockScope = true;
-  };
+CleanCss.prototype.sortMainScope = function () {
+  this.sortMainScope = true;
+};
 
-  CleanCss.prototype.sortMainScope = function () {
-    this.sortMainScope = true;
-  };
+CleanCss.prototype.value = function () {
+  var cssObject = capture(this, [], 0);
 
-  CleanCss.prototype.value = function () {
-    var cssObject = capture(this, [], 0);
-    sortCss(this, cssObject);
-    return getValue(this, cssObject);
-  };
+  sortCss(this, cssObject);
 
-  if (module) {
-    module.exports = function (string) {
-      return CleanCss(string);
-    };
-  }
-}());
+  return getValue(this, cssObject);
+};
+
+module.exports = function (string) {
+  return CleanCss(string);
+};
