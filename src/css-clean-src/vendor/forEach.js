9e79d8ad022c29c2016a14a1c033f5a4a14658a0
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-19 14:15:22 -0400

    Deprecated

--- src/css-clean-src/vendor/forEach.js
+++
@@ -1,5 +0,0 @@
-function forEach(array, fn) {
-  for (var i = 0, n = array.length; i < n; i++) {
-    fn(array[i], i, array);
-  }
-}
--- src/css-clean-src/vendor/partial.js
+++
@@ -1,7 +0,0 @@
-function partial(fn) {
-  var a = [].slice(fn, 1);
-  return function () {
-    var b = a.concat([].slice(arguments));
-    fn.apply(null, a);
-  }
-}
