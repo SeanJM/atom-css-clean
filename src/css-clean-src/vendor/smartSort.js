ab37ef20db833e979c48bf0ce963e3b0e4ccfd86
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-07-19 14:13:40 -0400

    cleaned up smartSort and required 'lasso', added 'module.exports'

--- src/css-clean-src/vendor/smartSort.js
+++ src/css-clean-src/vendor/smartSort.js
@@ -1,31 +1,43 @@
+const lasso = require('./lasso.min');
+
 function smartSort(property) {
   return function (a, b) {
-    var aC;
-    var bC;
+    let aC;
+    let bC;
+    let aa;
+    let bb;
+    let n;
+
     if (typeof property === 'string') {
       a = a[property];
       b = b[property];
     }
-  	var aa = lasso.matchType(a);
-    var bb = lasso.matchType(b);
-    var n = aa.length;
-    if (aa.length > bb.length) {
-    	n = bb.length;
-    }
+
+  	aa = lasso.matchType(a);
+    bb = lasso.matchType(b);
+
+    n = aa.length > bb.length
+      ? bb.length
+      : aa.length;
+
     for (var i = 0; i < n; i++) {
     	if (!isNaN(Number(aa[i])) && !isNaN(Number(bb[i]))) {
       	if (Number(aa[i]) > Number(bb[i])) {
         	return 1;
         } else if (Number(aa[i]) < Number(bb[i])) {
-  	      return -1
+  	      return -1;
         }
       }
+
     	if (aa[i] > bb[i]) {
       	return 1;
       } else if (aa[i] < bb[i]) {
       	return -1;
       }
     }
+
     return 0;
-  }
-};
+  };
+}
+
+module.exports = smartSort;
