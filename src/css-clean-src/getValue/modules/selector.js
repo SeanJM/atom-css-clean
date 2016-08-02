7f32abb1c983f70c6fa87959e752a97465a436bc
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-02 15:01:28 -0400

    ‘nested’ is required inside the function, this fixed a bug where ‘nested’ wasn’t defined yet.

--- src/css-clean-src/getValue/modules/selector.js
+++ src/css-clean-src/getValue/modules/selector.js
@@ -1,15 +1,22 @@
-const nested = require('./nested');
-
 function selector(settings, element, siblings) {
+  const nested = require('./nested');
+
   let tab = new Array((element.depth * settings.tabSize) + 1).join(settings.tabChar);
   let v = nested(settings, element, siblings);
 
   let selector = element.selector
-    .map((a, i) => i > 0 ? tab + a : a).join(',\n');
+    .map(
+      (a, i) => i > 0
+        ? tab + a
+        : a
+    )
+    .join(',\n');
 
   let content = v.length
     ? ' {\n' + v + tab + '}'
     : ' {}';
+
+  console.log(element.selector);
 
   return element.depth > 0
     ? siblings[0] === element
