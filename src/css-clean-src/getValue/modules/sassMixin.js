828925666782744b1f49028e71198b4dab17bee8
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-03 07:42:03 -0400

    fixed 'getValue' function for 'mixin' not returning contents

--- src/css-clean-src/getValue/modules/sassMixin.js
+++ src/css-clean-src/getValue/modules/sassMixin.js
@@ -1,10 +1,18 @@
-function sassMixin(that, element, parent) {
+function sassMixin(that, element, siblings) {
+  const nested = require('./nested');
+
   let tab = new Array((element.depth * that.tabSize) + 1).join(that.tabChar);
+  let v = nested(that, element, siblings);
+
   let args = element.arguments
-    ? '(' + element.arguments.join(', ') + ')'
+    ? `(${element.arguments.join(', ')})`
     : '';
 
-  return `${tab}${element.name} ${element.value}${args}`;
+  let content = v.length
+    ? `{\n${v}${tab}}`
+    : '{}';
+
+  return `${tab}${element.name} ${element.value}${args} ${content}`;
 }
 
 module.exports = sassMixin;
