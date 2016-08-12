ea291726be65e0e33e77d552609c865d28ab35ba
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-12 08:10:32 -0400

    Fixed sassEach capture

--- src/css-clean-src/capture/modules/sassEach.js
+++ src/css-clean-src/capture/modules/sassEach.js
@@ -1,14 +1,15 @@
 const nested = require('./nested');
 
 function sassEach(buffer) {
-  let c = nested(string, opt);
+  let c = nested(buffer);
   let m = c.arguments.split(' ');
 
   buffer.string = buffer.string.substr(c.length);
 
   return {
     name : m[0],
-    value : m.slice(1).map(a => a.trim()).filter(a => a.length).join(' ')
+    value : m.slice(1).map(a => a.trim()).filter(a => a.length).join(' '),
+    content : c.content
   };
 }
 
