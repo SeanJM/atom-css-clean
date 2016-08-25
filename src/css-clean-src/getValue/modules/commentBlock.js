5d26d8f92c8739795582d768819c2d555ad6f82d
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-08-25 08:29:30 -0400

    Added support for lists in comments and fixed nested special comments and section comments

--- src/css-clean-src/getValue/modules/commentBlock.js
+++ src/css-clean-src/getValue/modules/commentBlock.js
@@ -1,23 +1,82 @@
 const lasso = require('lasso-string');
 
+function isSpecialComment(element) {
+  return /(\s+|)!/.test(element.value[0]);
+}
+
+function isSectionTitle(element) {
+  /*------------------------------------*\
+    #SECTION-TITLE
+  \*------------------------------------*/
+  return (
+    /^[\-]+(\s+|)\*\\$/.test(element.value[0])
+    && /^(\s+|)\\\*(\s+|)[\-]+$/.test(element.value.slice(-1)[0])
+  );
+}
+
+function isEmptyLine(wordList) {
+  return (
+    wordList
+    && wordList.length === 1
+    && wordList[0].length === 0
+  );
+}
+
 function splitByLineBreak(that, element) {
   let lines = [];
   let lineIndex = 0;
   let tabLength = that.getTab(element.depth + 2).length;
 
   let raw = element.value
-    .map(a => a.replace(/^(\s+|)\*/g, ''))
+    .map(a => a
+      .replace(/^(\s+|)\*/g, '')
+      .replace(/^(\s+)([^\s])/, '$2')
+      .replace(/([^\s])(\s+)$/, '$1')
+    )
+    .reduce(function (a, b) {
+      var o = [a];
+
+      if (Array.isArray(a)) {
+        o = a;
+      } else if (!a.length) {
+        o = [];
+      }
+
+      if ('- ' === b.substr(0, 2)) {
+        o.push('');
+      }
+
+      o.push(b);
+      return o;
+    });
+
+  // Single line
+  if (typeof raw === 'string') {
+    raw = [raw];
+  }
+
+  raw = raw
     .join(' ')
     .trim()
     .split(' ');
 
-  raw.forEach(function (word) {
+  raw.forEach(function (word, i) {
+    let nextWord = raw[i + 1];
+
     if (!lines[lineIndex]) {
       lines[lineIndex] = [];
     } else if (
       tabLength + lines[lineIndex].join(' ').length + word.length
       > that.lineBreak
     ) {
+      lineIndex++;
+      lines[lineIndex] = [];
+    } else if (!word.length && !isEmptyLine(lines[lineIndex])) {
+      // is a list
+      if (nextWord !== '-') {
+        lineIndex++;
+        lines[lineIndex] = [];
+      }
       lineIndex++;
       lines[lineIndex] = [];
     }
@@ -33,12 +92,14 @@ function formatSectionTitle(that, element) {
     let $tab = '';
 
     if (i > 0) {
-      $tab = that.getTab(element.depth);
+      $tab = tab + that.getTab(1);
     }
+
+    line = line.trim();
 
     return i < element.value.length - 1
       ? $tab + line + '\n'
-      : $tab + line;
+      : tab + line;
   });
   return '/*' + value.join('') + '*/';
 }
@@ -46,17 +107,17 @@ function formatSectionTitle(that, element) {
 function formatSpecialComment(that, element) {
   const tab = that.getTab(element.depth);
 
-  return '/*' + (
-    element.value.length > 1
-      ? element.value.map(function (line, i) {
-        let $tab = '';
-        if (i > 0) {
-          $tab = tab + that.getTab(element.depth + 1);
-        }
-        return $tab + line.trim();
-      }).join('\n') + '\n'
-      : element.value.join('\n') + ' '
-    ) + '*/';
+  if (element.value.length === 1) {
+    return '/*' + element.value.join('\n') + ' */';
+  }
+
+  return '/*' + element.value.map(function (line, i) {
+    let $tab = '';
+    if (i > 0) {
+      $tab = tab + that.getTab(1);
+    }
+    return $tab + line.trim();
+  }).join('\n') + '\n' + tab + ' */';
 }
 
 function formatDefault(that, element) {
@@ -75,20 +136,6 @@ function formatDefault(that, element) {
 
     '\n' + tab + ' */'
   : '/* ' + element.value.join('') + ' */';
-}
-
-function isSpecialComment(element) {
-  return /(\s+|)!/.test(element.value[0]);
-}
-
-function isSectionTitle(element) {
-  /*------------------------------------*\
-    #SECTION-TITLE
-  \*------------------------------------*/
-  return (
-    /^[\-]+(\s+|)\*\\$/.test(element.value[0])
-    && /^\\\*(\s+|)[\-]+$/.test(element.value.slice(-1)[0])
-  );
 }
 
 function commentBlock(that, element) {
